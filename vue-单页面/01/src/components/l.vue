32bb409cb5d0559c9b8c3d928e8f346fc58bcf99
jihaobin <108733821+jihaobin@users.noreply.github.com>
2022-10-25 20:10:57 +0800

    Add files via upload

---
+++ vue-单页面/01/src/components/l.vue
@@ -0,0 +1,40 @@
+<script>
+import lesson from "./lesson/lessonSon.vue";
+export default {
+  components: { lesson },
+  data() {
+    return {
+      lessons: [
+        { id: 1, name: "javascript", img: "/js.png" },
+        { id: 2, name: "vue", img: "/vue3.jpg" },
+        { id: 3, name: "tapescript", img: "/ts.jpg" },
+      ],
+    };
+  },
+  methods: {
+    del(index) {
+      this.lessons.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="lesson">
+    <lesson
+      v-for="(item, index) in lessons"
+      :lesson="item"
+      :index="index"
+      :key="item.id"
+      @del="del"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+.lesson {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 20px;
+}
+</style>
---
+++ vue-单页面/01/src/components/lesson/lessonSon.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <span @click="$emit('del', index)">X</span>
+    <img :src="lesson.img" />
+    <h3>{{ lesson.name }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["lesson", "index"],
+  emits: ["del"],
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  text-align: center;
+  border: 1px solid #ddd;
+  transition: 1s;
+  position: relative;
+  &:hover {
+    box-shadow: 0 0 20px #aaa;
+    > span {
+      opacity: 1;
+    }
+  }
+  h3 {
+    padding: 0 0 20px;
+    margin: 0;
+  }
+  img {
+    width: 100%;
+  }
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    width: 30px;
+    height: 30px;
+    background: gray;
+    color: white;
+    border-radius: 100%;
+    transition: 0.5s;
+    opacity: 0;
+  }
+}
+</style>
