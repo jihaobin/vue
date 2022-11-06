73179b4ac8f624bb754fc4798f4a82eff6c9e1e6
jihaobin <2644263665@qq.com>
2022-11-06 16:18:44 +0800

    添加组合式API练习

---
+++ vue-单页面/03/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <compostiveVue ref="test" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import compostiveVue from "./components/compostive.vue";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue-单页面/03/src/components/compostive.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="pre">-</button>
+    {{ content }}
+    <button @click="add">+</button>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+export default {
+  setup(props) {
+    let content = ref(0);
+
+    function add() {
+      content.value++;
+    }
+    function pre() {
+      content.value--;
+    }
+
+    watch(content, (v) => {
+      v < 0 ? (content.value = 0) : "";
+    });
+
+    return { content, add, pre };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
